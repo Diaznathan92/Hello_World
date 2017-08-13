8cd54a2164ad938f5185f8b3428d8a85a048908f
Diaznathan92 <Diaz.nathan92@gmail.com>
2017-08-13 08:17:23 -0400

    Added Files
    Added the initial files on my machine

---
+++ Hello_World_GitHub/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var mySQL = require('MySQL');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/home',function(req,res,next){
+    var query = 'Select 1 from list_db.user_info';
+
+    var con = mySQL.createConnection({
+    host: "localhost",
+    user: "root"
+    });
+   
+    con.connect(function(err) {
+    if (err) throw err;
+    con.query(query, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.render('index', {title: result[0].Username});
+    });
+    });
+});
+module.exports = router;
